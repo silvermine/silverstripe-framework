76edfa0374c4285bed04048c7b81fb9a98f0079c
Damian Mooyman <damian@silverstripe.com>
2016-01-26 10:27:52 +1300

    API remove obsolete class loaders from test listeners

--- dev/SapphireTestReporter.php
+++ dev/SapphireTestReporter.php
@@ -1,6 +1,8 @@
 <?php
-if(!class_exists('PHPUnit_Framework_TestResult', false)) require_once 'PHPUnit/Framework/TestResult.php';
-if(!class_exists('PHPUnit_Framework_TestListener', false)) require_once 'PHPUnit/Framework/TestListener.php';
+
+if(!interface_exists('PHPUnit_Framework_TestListener')) {
+	return;
+}
 
 /**#@+
  * @var int
